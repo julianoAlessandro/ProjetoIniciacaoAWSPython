ee0ab62bd7d073289e2890df5fe1d66143c4614f
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2024-03-06 16:45:41 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from Classes.SeleniumAutomatizar import SeleniumAutomatizar as SA
+automacao = SA()
+driver = automacao.get_driver('https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados/enade')
+
+
+
+
+
